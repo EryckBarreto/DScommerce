9243f7b8838d1ddce4fded11802b128589130938
Eryck Barreto <eryck.barreto@hotmail.com>
2024-01-20 15:22:26 -0300

    implementada a rota get product by id

--- dscommerce/dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ dscommerce/dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,16 +1,28 @@
 package com.devsuperior.dscommerce.controllers;
 
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-    @GetMapping
-    public String teste() {
-        return "Olá Mundo!";
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/{id}")
+    public ProductDTO findById(@PathVariable Long id) {
+//        ProductDTO dto = service.findById(id);
+//        return dto;
+        return service.findById(id);
     }
 
 }
